880ff20f907ca83c88a6a3c539f2049aec4f0c7c
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-10-08 01:05:54 -0400

    Formatting and function updates

--- emp-zk/emp-utills/bit_utils.h
+++ emp-zk/emp-utills/bit_utils.h
@@ -5,79 +5,95 @@
 #include <emp-zk/emp-zk.h>
 
 using namespace std;
-namespace emp {
+namespace emp
+{
 
   template <size_t N1, size_t N2>
-    bitset<N1 + N2> concat(const bitset < N1 > & b1,
-      const bitset<N2> & b2) {
-      string s1 = b1.to_string();
-      string s2 = b2.to_string();
-      return bitset<N1 + N2> (s1 + s2);
-    }
+  bitset<N1 + N2> concat(const bitset<N1> &b1, const bitset<N2> &b2)
+  {
+    string s1 = b1.to_string();
+    string s2 = b2.to_string();
+    return bitset<N1 + N2>(s1 + s2);
+  }
 
-  void concat_bits(int n1, int n2, bool * witness) {
-    std::bitset <32> bits1(n1);
-    std::bitset <32> bits2(n2);
-    //cout<<bits1<<endl;
+  void concat_bits(int n1, int n2, bool *witness)
+  {
+    std::bitset<32> bits1(n1);
+    std::bitset<32> bits2(n2);
+    // cout<<bits1<<endl;
     auto out_bits = concat(bits1, bits2);
 
-    for (size_t i = 0; i < out_bits.size(); i++) {
+    for (size_t i = 0; i < out_bits.size(); i++)
+    {
       witness[i] = out_bits.test(i);
     }
   }
 
-  void hash_witness(bool * output, bool * witness, string filename) {
-    vector <Bit> W, O;
+  void hash_witness(bool *output, bool *witness, string filename)
+  {
+    vector<Bit> W, O;
     for (int i = 0; i < 512; ++i)
       W.push_back(Bit(witness[i], ALICE));
     O.resize(512);
-    for (int i = 0; i < 512; ++i) O[i] = Bit(false, PUBLIC);
+    for (int i = 0; i < 512; ++i)
+      O[i] = Bit(false, PUBLIC);
     BristolFormat cf(filename.c_str());
-    cf.compute((block * ) O.data(), (block * ) W.data(), nullptr);
+    cf.compute((block *)O.data(), (block *)W.data(), nullptr);
     for (int i = 0; i < 64; ++i)
       cf.compute(O.data(), O.data(), nullptr);
-    for (int i = 0; i < 256; ++i) {
-      bool tmp = O[i].reveal < bool > (PUBLIC);
-      //if(tmp != output[i])
-      //        error("wrong");
+    for (int i = 0; i < 256; ++i)
+    {
+      bool tmp = O[i].reveal<bool>(PUBLIC);
+      // if(tmp != output[i])
+      //         error("wrong");
       output[i] = tmp;
     }
   }
 
-  void expand_to(bool * source_one, bool * source_two, bool * dest, int init_size, int target_size) { //edit this to take two nodes and make them into one witness
-    if (target_size != (init_size * 2)) {
+  void expand_to(bool *source_one, bool *source_two, bool *dest, int init_size, int target_size)
+  { // edit this to take two nodes and make them into one witness
+    if (target_size != (init_size * 2))
+    {
       error("leaves must be same size");
     }
-    for (int i = 0; i < init_size; i++) {
+    for (int i = 0; i < init_size; i++)
+    {
       bool temp_bit = source_one[i];
       dest[i] = temp_bit;
     }
     int indx_counter = init_size;
-    for (int j = init_size; j < target_size; j++) {
+    for (int j = init_size; j < target_size; j++)
+    {
       bool temp_bit_again = source_two[init_size - indx_counter];
       dest[j] = temp_bit_again;
       indx_counter--;
     }
-
   }
 
-  void bitwise_add(bool * num_one, bool * num_two, bool * res, int new_size) {
+  void bitwise_add(bool *num_one, bool *num_two, bool *res, int new_size)
+  {
     bool carry = 0;
 
-    for (int i = 0; i < new_size; i++) {
+    for (int i = 0; i < new_size; i++)
+    {
       bool answ = num_one[i] ^ num_two[i];
-      switch (carry) {
+      switch (carry)
+      {
       case 1:
-        switch (answ) {
+        switch (answ)
+        {
         case 1:
           res[i] = 0;
           carry = 1;
           break;
         default:
-          if (num_one[i] && num_two[i] == 1) {
+          if (num_one[i] && num_two[i] == 1)
+          {
             res[i] = 1;
             carry = 1;
-          } else {
+          }
+          else
+          {
             res[i] = 1;
             carry = 0;
           }
@@ -90,104 +106,244 @@ namespace emp {
         break;
       }
     }
-    if (carry == 1) {
+    if (carry == 1)
+    {
       error("overflow\n");
     }
   }
-  int compare(bool * comparing, bool * compare_to, int size) { //use & as inputs for bit arrays
+  int compare(bool *comparing, bool *compare_to, int size)
+  { // use & as inputs for bit arrays
     int sum_1 = 0;
     int sum_2 = 0;
-    for (int i = 0; i < size; i++) {
-      if (comparing[i] == true) {
+    for (int i = 0; i < size; i++)
+    {
+      if (comparing[i] == true)
+      {
         sum_1 += pow(2, i);
       }
-      if (compare_to[i] == true) {
+      if (compare_to[i] == true)
+      {
         sum_2 += pow(2, i);
       }
     }
-    if (sum_1 < sum_2) {
+    if (sum_1 < sum_2)
+    {
       return 1;
-    } //first bool array is less than second input array
-    else if (sum_1 == sum_2) {
+    } // first bool array is less than second input array
+    else if (sum_1 == sum_2)
+    {
       return 2;
-    } //first is equal to second
-    else {
+    } // first is equal to second
+    else
+    {
       return 3;
-    } //first is greater than second
+    } // first is greater than second
   }
 
-  void inner_sort_mark_II(bool ** leaves, int size, int bit_size) {
-    vector <int> nums;
-    for (int i = 0; i < size; i++) {
+  void inner_sort(bool **leaves, vector<bool *> &Left, vector<bool *> &Right, int start, int end, int full_size)
+  {
+
+    // vector<bool*> Left, Right;
+    int length = end - start;
+    cout << "Start = " << start << endl;
+    cout << "End = " << end << endl;
+    cout << "Length = " << length << endl;
+    if (length > 1)
+    {
+      srand(time(NULL));
+      int pivot = rand() % length;
+      pivot += start;
+      cout << "Pivot = " << pivot << endl;
+      for (int i = start; i < pivot; i++)
+      {
+        int res = compare(leaves[i], leaves[pivot], 256);
+        switch (res)
+        {
+        case 1:
+          Left.push_back(leaves[i]);
+          // cout<<"less than"<<endl;
+          break;
+        case 2:
+          Right.push_back(leaves[i]);
+          // cout<<"equal"<<endl;
+          break;
+        default:
+          Right.push_back(leaves[i]);
+          // cout<<"more than"<<endl;
+          break;
+        }
+      }
+
+      for (int i = pivot + 1; i < end; i++)
+      {
+        int res = compare(leaves[i], leaves[pivot], 256);
+        switch (res)
+        {
+        case 1:
+          Left.push_back(leaves[i]);
+          // cout<<"less than"<<endl;
+          break;
+        case 2:
+          Right.push_back(leaves[i]);
+          // cout<<"equal"<<endl;
+          break;
+        default:
+          Right.push_back(leaves[i]);
+          // cout<<"more than"<<endl;
+          break;
+        }
+      }
+
+      bool *temp = leaves[pivot];
+      int mark = Left.size();
+      for (int i = start; i < mark; i++)
+      {
+        leaves[i] = Left[i];
+      }
+      leaves[mark] = temp;
+      for (int i = mark + 1; i < end; i++)
+      {
+        int right_vec_indx = i - (mark + 1);
+        leaves[i] = Right[right_vec_indx];
+      }
+
+      Left.clear();
+      Right.clear();
+      /*
+      for(unsigned int j = 0; j<8;j++){
+                          for(int k=0;k<7;k++){
+                                  cout<<leaves[j][k];
+                          }
+                          cout<<endl;
+                  }
+      */
+      cout << "Mark = " << mark << endl;
+      if (mark == 0)
+      { // if left was empty
+        inner_sort(leaves, Left, Right, start + 1, end, full_size);
+      }
+      else if (mark == length)
+      { // if right was empty
+        inner_sort(leaves, Left, Right, start, end - 1, full_size);
+      }
+      else
+      {
+        inner_sort(leaves, Left, Right, start, mark - 1, full_size);
+        inner_sort(leaves, Left, Right, mark + 1, end, full_size);
+      }
+
+      // somtimes works, but mostly leads to infinte loops and core dumps
+    }
+    else if (length == 1)
+    {
+      int res_comp = compare(leaves[start], leaves[end], 256);
+      bool *tmp = leaves[end];
+      switch (res_comp)
+      {
+      case 3:
+        leaves[end] = leaves[start];
+        leaves[start] = tmp;
+        break;
+      default:
+        break;
+      }
+    }
+    else
+    {
+      cout << "sorting branch has ended" << endl;
+    }
+  }
+  void inner_sort_mark_II(bool **leaves, int size, int bit_size)
+  {
+    vector<int> nums;
+    for (int i = 0; i < size; i++)
+    {
       int number = 0;
-      for (int j = 0; j < bit_size; j++) {
-        if (leaves[i][j] == 1) {
+      for (int j = 0; j < bit_size; j++)
+      {
+        if (leaves[i][j] == 1)
+        {
           number += pow(2, j);
         }
-
       }
 
       nums.push_back(number);
-
     }
     int total_nums = nums.size();
     std::sort(nums.begin(), nums.end());
-    for (int leaves_indx = 0; leaves_indx < total_nums; leaves_indx++) {
-      std::bitset <32> bit_from_vec(nums[leaves_indx]);
-      for (size_t bt = 0; bt < bit_from_vec.size(); bt++) {
+    for (int leaves_indx = 0; leaves_indx < total_nums; leaves_indx++)
+    {
+      std::bitset<32> bit_from_vec(nums[leaves_indx]);
+      for (size_t bt = 0; bt < bit_from_vec.size(); bt++)
+      {
         leaves[leaves_indx][bt] = bit_from_vec.test(bt);
       }
-
     }
   }
 
-  void sort_leaves(bool ** incoming_leaves, int size, int bit_size) {
+  void sort_leaves(bool **incoming_leaves, int size, int bit_size)
+  {
     inner_sort_mark_II(incoming_leaves, size, bit_size);
+    /*for(int i = 0; i<size; i++){
+                  for(int j = 0; j<10;j++){
+                          cout<<incoming_leaves[i][j];
+                  }
+                  cout<<endl;
+          }*/
   }
 
-  std::vector <bool> toBooleanArray(unsigned long long num) {
-    std::bitset < 256 > b(num);
+  std::vector<bool> toBooleanArray(unsigned long long num)
+  {
+    std::bitset<256> b(num);
     std::cout << num << std::endl;
-    std::vector < bool > arr(256);
-    for (int i = 0; i < 256; ++i) {
+    std::vector<bool> arr(256);
+    for (int i = 0; i < 256; ++i)
+    {
       arr[i] = b[i];
     }
     return arr;
   }
 
-  std::vector<std::vector<bool>> readDataset(const std::string & filename) {
-    std::vector < std::vector < bool >> results;
+  std::vector<std::vector<bool>> readDataset(const std::string &filename)
+  {
+    std::vector<std::vector<bool>> results;
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
       std::cerr << "Failed to open file: " << filename << std::endl;
       return results;
     }
 
     unsigned long long num;
-    while (file >> num) {
+    while (file >> num)
+    {
       results.push_back(toBooleanArray(num));
     }
     file.close();
     return results;
   }
 
-  std::vector<bool> convertEntryToBooleanArray(const std::string & filename, size_t entryIndex) {
+  std::vector<bool> convertEntryToBooleanArray(const std::string &filename, size_t entryIndex)
+  {
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
       std::cerr << "Failed to open file: " << filename << std::endl;
-      return std::vector < bool > ();
+      return std::vector<bool>();
     }
-
+    std::string addr;
     unsigned long long num;
     size_t currentIndex = 0;
-    while (file >> num) {
-      if (currentIndex == entryIndex) {
+    while (file >> addr >> num)
+    {
+      if (currentIndex == entryIndex)
+      {
         return toBooleanArray(num);
       }
       ++currentIndex;
     }
 
     std::cerr << "Entry index out of bounds" << std::endl;
-    return std::vector < bool > ();
+    return std::vector<bool>();
   }
 }
--- emp-zk/emp-utills/merkle_tree.h
+++ emp-zk/emp-utills/merkle_tree.h
@@ -1,238 +1,212 @@
 #ifndef EMP_MERKLE_H__
 #define EMP_MERKLE_H__
 
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <emp-zk/emp-zk.h>
-#include "emp-tool/emp-tool.h"
+#include <iostream>
+#include <emp-tool/emp-tool.h>
+using namespace emp;
+using namespace std;
 
-namespace emp
+class MerkleTree
 {
-    class MerkleTree
+    int num_leaves;
+    int levels;
+    bool ***tree;
+
+public:
+    MerkleTree(int num_leaves, int levels, bool **bit_array, string filename);
+    void leaf_print(int level, int leaf);
+    void init_verify_path(int **tree_path); // use once
+    void get_verify_path(int *tree_path, int index);
+    void prove_in_tree(bool *leaf, int *path);
+    void prove_not_in_tree(bool *non_leaf);
+};
+
+MerkleTree::MerkleTree(int num_leaves, int levels, bool **bit_array, string filename)
+{
+    this->num_leaves = num_leaves;
+    this->levels = levels;
+
+    if ((num_leaves == 0) || ((num_leaves & (num_leaves - 1)) != 0))
     {
+        error("number of leaves is not a power of 2\n");
+    }
 
-    public:
-        int num_leaves;
-        int levels;
-        bool ***tree;
-        MerkleTree(int num_leaves, int levels, bool **bit_array, string filename);
-        void leaf_print(int level, int leaf);
-        void init_verify_path(int **tree_path); // use once
-        void get_verify_path(int *tree_path, int index);
-        void prove_in_tree(bool *leaf, int *path);
-        void prove_not_in_tree(bool *non_leaf);
-        void serialize(const std::string &filename);
-        MerkleTree* deserialize(const std::string &filename);
-    };
-
-    MerkleTree::MerkleTree(int num_leaves, int levels, bool **bit_array, string filename)
+    if (num_leaves != pow(2, levels - 1))
     {
-        this->num_leaves = num_leaves;
-        this->levels = levels;
-
-        if ((num_leaves == 0) || ((num_leaves & (num_leaves - 1)) != 0))
+        error("wrong number of levels for leaf cardinality\n");
+    }
+    tree = new bool **[levels];
+    tree[0] = new bool *[num_leaves];
+    for (int i = 0; i < levels; i++)
+    {
+        int nodes = pow(2, levels - (i + 1));
+        tree[i] = new bool *[nodes];
+        for (int j = 0; j < nodes; j++)
         {
-            error("number of leaves is not a power of 2\n");
-        }
-
-        if (num_leaves != pow(2, levels - 1))
-        {
-            error("wrong number of levels for leaf cardinality\n");
-        }
-        tree = new bool **[levels];
-        tree[0] = new bool *[num_leaves];
-        for (int i = 0; i < levels; i++)
-        {
-            int nodes = pow(2, levels - (i + 1));
-            tree[i] = new bool *[nodes];
-            for (int j = 0; j < nodes; j++)
-            {
-                tree[i][j] = new bool[256];
-            }
-        }
-
-        for (int leaf = 0; leaf < num_leaves; leaf++)
-        {
-            tree[0][leaf] = bit_array[leaf];
-        }
-
-        for (int lvl = 1; lvl < levels; lvl++)
-        {
-            int lvl_node_count = pow(2, levels - (lvl + 1));
-            int pairing[2]{0, 1};
-            for (int nd = 0; nd < lvl_node_count; nd++)
-            {
-                bool *wit = new bool[512];
-                expand_to(tree[lvl - 1][pairing[0]], tree[lvl - 1][pairing[1]], wit, 256, 512);
-                bool *output = new bool[256];
-                hash_witness(output, wit, filename);
-                tree[lvl][nd] = output;
-                pairing[0] += 2;
-                pairing[1] += 2;
-            }
+            tree[i][j] = new bool[256];
         }
     }
 
-    void MerkleTree::leaf_print(int level, int leaf)
+    for (int leaf = 0; leaf < num_leaves; leaf++)
     {
-        for (int i = 0; i < 256; i++)
-        {
-            std::cout << tree[level][leaf][i];
-        }
-        std::cout << std::endl;
+        tree[0][leaf] = bit_array[leaf];
     }
 
-    void MerkleTree::init_verify_path(int **tree_path)
+    for (int lvl = 1; lvl < levels; lvl++)
     {
-        *tree_path = new int[levels];
-    }
-
-    void MerkleTree::get_verify_path(int *tree_path, int index)
-    {
-        tree_path[0] = index;
-        for (int i = 1; i < levels; i++)
+        int lvl_node_count = pow(2, levels - (lvl + 1));
+        int pairing[2]{0, 1};
+        for (int nd = 0; nd < lvl_node_count; nd++)
         {
-            int child_id = tree_path[i - 1];
-            int parent_id = child_id / 2;
-            if (parent_id >= pow(2, levels - (i + 1)))
-            {
-                for (int j = i; j < levels; j++)
-                {
-                    tree_path[j] = 9;
-                }
-                break;
-            }
-            else
-            {
-                tree_path[i] = parent_id;
-            }
+            bool *wit = new bool[512];
+            expand_to(tree[lvl - 1][pairing[0]], tree[lvl - 1][pairing[1]], wit, 256, 512);
+            bool *output = new bool[256];
+            hash_witness(output, wit, filename);
+            tree[lvl][nd] = output;
+            pairing[0] += 2;
+            pairing[1] += 2;
         }
     }
+}
 
-    void MerkleTree::prove_in_tree(bool *leaf, int *path)
+void MerkleTree::leaf_print(int level, int leaf)
+{
+    for (int i = 0; i < 256; i++)
     {
-        bool leaf_flag = 0;
-        for (int i = 0; i < num_leaves; i++)
-        {
-            int res = compare(leaf, tree[0][i], 256);
-            switch (res)
-            {
-            case 2:
-                this->get_verify_path(path, i);
-                leaf_flag = 1;
-                break;
-            default:
-                break;
-            }
-        }
-        if (leaf_flag == 0)
-        {
-            std::cout << "Leaf not in tree" << std::endl;
-            return;
-        }
-        else if (path[levels - 1] < 0)
-        {
-            std::cout << "leaf does not have path to root" << std::endl;
-            return;
-        }
-        std::cout << "Leaf found" << std::endl;
+        cout << tree[level][leaf][i];
     }
+    cout << endl;
+}
 
-    void MerkleTree::prove_not_in_tree(bool *non_leaf)
+void MerkleTree::init_verify_path(int **tree_path)
+{
+    *tree_path = new int[levels];
+}
+
+void MerkleTree::get_verify_path(int *tree_path, int index)
+{
+    tree_path[0] = index;
+    for (int i = 1; i < levels; i++)
     {
-        int *left_path = new int[levels];
-        int *right_path = new int[levels];
-        int proof_flag = 1; // assume leaf is in tree
-        int match_flag = 0;
-        for (int i = 0; i < num_leaves; i++)
+        int child_id = tree_path[i - 1];
+        int parent_id = child_id / 2;
+        if (parent_id >= pow(2, levels - (i + 1)))
         {
-            int res = compare(non_leaf, tree[0][i], 256);
-            if (res == 2)
+            for (int j = i; j < levels; j++)
             {
-                match_flag = 1;
+                tree_path[j] = 9;
             }
-        }
-        if (match_flag > 0)
-        {
-            std::cout << "leaf found in tree" << std::endl;
+            break;
         }
         else
         {
-            int prev = 0;
-            for (int i = 0; i < num_leaves; i++)
+            tree_path[i] = parent_id;
+        }
+    }
+}
+
+void MerkleTree::prove_in_tree(bool *leaf, int *path)
+{
+    bool leaf_flag = 0;
+    for (int i = 0; i < num_leaves; i++)
+    {
+        int res = compare(leaf, tree[0][i], 256);
+        // cout<<"res = "<<res<<endl;
+        switch (res)
+        {
+        case 2:
+            this->get_verify_path(path, i);
+            leaf_flag = 1;
+            break;
+        default:
+            break;
+        }
+    }
+    if (leaf_flag == 0)
+    {
+        cout << "Leaf not in tree" << endl;
+    }
+    else if (path[levels - 1] < 0)
+    {
+        cout << "leaf does not have path to root" << endl;
+    }
+    else
+    {
+        cout << "leaf does have path to root" << endl;
+        for (int level_index = 0; level_index < levels - 1; level_index++)
+        {
+            cout << path[level_index] << "->";
+        }
+        cout << path[levels - 1];
+    }
+    cout << endl;
+}
+
+void MerkleTree::prove_not_in_tree(bool *non_leaf)
+{
+    int *left_path = new int[levels];
+    int *right_path = new int[levels];
+    int proof_flag = 1; // assume leaf is in tree
+    int match_flag = 0;
+    for (int i = 0; i < num_leaves; i++)
+    {
+        int res = compare(non_leaf, tree[0][i], 256);
+        if (res == 2)
+        {
+            match_flag = 1;
+        }
+    }
+    if (match_flag > 0)
+    {
+        cout << "leaf found in tree" << endl;
+    }
+    else
+    {
+        int prev = 0;
+        for (int i = 0; i < num_leaves; i++)
+        {
+            int res2 = compare(non_leaf, tree[0][i], 256);
+            if (res2 == 1 && prev == 3)
             {
-                int res2 = compare(non_leaf, tree[0][i], 256);
-                if (res2 == 1 && prev == 3)
+                this->get_verify_path(left_path, i - 1);
+                this->get_verify_path(right_path, i);
+                if (left_path[0] == (right_path[0] - 1))
                 {
-                    this->get_verify_path(left_path, i - 1);
-                    this->get_verify_path(right_path, i);
-                    if (left_path[0] == (right_path[0] - 1))
-                    {
-                        proof_flag = 0;
-                    }
+                    proof_flag = 0;
                 }
-                prev = res2;
             }
-        }
-
-        if (proof_flag == 0)
-        {
-            std::cout << "leaf is proven not to be in tree" << std::endl;
-        }
-        else if (proof_flag == 1 && match_flag == 0)
-        {
-            std::cout << "leaf value exceeded one of the extremes of the tree leaves" << std::endl;
-        }
-    }
-    void MerkleTree::serialize(const std::string &filename) {
-        std::ofstream outFile(filename);
-        if (!outFile) {
-            std::cerr << "Error opening file for writing: " << filename << std::endl;
-            return;
-        }
-
-        outFile << num_leaves << std::endl;
-        outFile << levels << std::endl;
-
-        for (int i = 0; i < levels; i++) {
-            int nodes = pow(2, levels - (i + 1));
-            for (int j = 0; j < nodes; j++) {
-                for(int k = 0; k < 256; k++) {
-                outFile << tree[i][j][k];
-            }
-            outFile << "" << std::endl;
-        }
-        }
-        outFile.close();
-    }
-MerkleTree* MerkleTree::deserialize(const std::string &filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file: " << filename << std::endl;
-        return nullptr;
-    }
-
-    int num_leaves, levels;
-    file >> num_leaves;
-    file >> levels;
-    bool **bit_array;
-    bit_array = new bool *[num_leaves];
-    for (int i = 0; i < num_leaves; ++i) {
-        bit_array[i] = new bool[256];
-        std::string line;
-        file >> line;
-        if (line.length() != 256) {
-            std::cerr << "Error: Expected line of length 256, but got " << line.length() << std::endl;
-        }
-        for (int j = 0; j < 256; ++j) {
-            bit_array[i][j] = (line[j] == '1');
+            prev = res2;
         }
     }
 
-    file.close();
-
-    return new MerkleTree(num_leaves, levels, bit_array, filename);
+    if (proof_flag == 0)
+    {
+        cout << "leaf is proven not to be in tree" << endl;
+    }
+    else if (proof_flag == 1 && match_flag == 0)
+    {
+        cout << "leaf value exceeded one of the extremes of the tree leaves" << endl;
+    }
 }
+
+void dynamic_leaf_array_init(int num_of_leaves, bool **leaf_array)
+{
+    std::vector<vector<bool>> bits_array(num_of_leaves);
+    for (int i = 0; i < num_of_leaves; i++)
+    {
+        bits_array[i] = convertEntryToBooleanArray("out.txt", i);
+    }
+
+    for (size_t lf = 0; lf < num_of_leaves; lf++)
+    {
+        bool *num = new bool[256];
+        for (int bt = 0; bt < 256; bt++)
+        {
+            num[bt] = bits_array[lf][bt];
+        }
+        leaf_array[lf] = num;
+    }
 }
+
 #endif
